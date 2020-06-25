265f8940abc3ef714656167d9fd162d666ccc7da
M.Gergo <gergo@bmrg.hu>
2020-06-25 08:00:03 +0200

    Rev: 4662

--- mayor-orig/mayor-naplo/www/policy/private/naplo/admin/debug.php
+++ mayor-orig/mayor-naplo/www/policy/private/naplo/admin/debug.php
@@ -1,6 +1,10 @@
 <?php
 global $ADAT;
 
+    echo '<style type="text/css">
+/*	div.mayorbody { color:white; background-color: #888; padding:10px;}*/
+    </style>';
+
     if (is_array($ADAT)) {
 	foreach ($ADAT['debug_result'] as $key => $value) {
 	    echo '<h1>'.$key.'</h1>';
