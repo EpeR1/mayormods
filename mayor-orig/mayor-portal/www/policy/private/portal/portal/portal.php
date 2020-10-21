a5ac3d0ab4015ed2683706cd0677ce6ce159f0ed
M.Gergo <gergo@bmrg.hu>
2020-10-21 20:35:04 +0200

    Rev: 4691

--- mayor-orig/mayor-portal/www/policy/private/portal/portal/portal.php
+++ mayor-orig/mayor-portal/www/policy/private/portal/portal/portal.php
@@ -3,6 +3,7 @@
     global $skin;
 
     /* $skinnek lennie kell */                                                                                                                                                                             
+
     if (defined('_POLICY') && defined('__PORTAL_CODE') && file_exists('policy/'._POLICY.'/portal/portal/portal_'.__PORTAL_CODE.'.'.$skin.'.php') ) {                                                       
         require('policy/'._POLICY.'/portal/portal/portal_'.__PORTAL_CODE.'.'.$skin.'.php');                                                                                                                
     } elseif (file_exists('policy/'._POLICY.'/portal/portal/portal_'.demo.'.'.$skin.'.php')) {                                                                                                             
